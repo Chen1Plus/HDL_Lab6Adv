fee28d85729ead27aa517a05e738e4fb05a6bf73
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-11-26 01:26:53 +0800

    refactor: sonic clock and trigger

--- src/hdl/sonic.v
+++ src/hdl/sonic.v
@@ -2,38 +2,54 @@
 // clk = 100MHz
 // <Trig> and <Echo> should connect to the sensor
 // <distance> is the output distance in cm
-module sonic_top(clk, rst, Echo, Trig, distance);
-	input clk, rst, Echo;
-	output Trig;
-    output [19:0] distance;
+module sonic_top (
+    input clk,
+    input rst,
+    input Echo,
+    output Trig,
+    output [19:0] distance
+);
+    wire [19:0] dis;
 
-	wire[19:0] dis;
-    wire clk1M;
-	wire clk_2_17;
+    wire c1MHz, c8MHz;
+
+    Clk8MHz c0 (
+        .reset(rst),
+        .clk  (clk),
+        .c8MHz(c8MHz)
+    );
+
+    ClkDivider c1 (
+        .rst    (rst),
+        .clk    (clk),
+        .clk_div(c1MHz)
+    );
+
+    wire clk_2_17;
 
     assign distance = dis;
 
-    div clk1(clk ,clk1M);
-	TrigSignal u1(.clk(clk), .rst(rst), .trig(Trig));
-	PosCounter u2(.clk(clk1M), .rst(rst), .echo(Echo), .distance_count(dis));
- 
+    SonicTrigger u1(.c1MHz(c1MHz), .rst(rst), .trig(Trig));
+    PosCounter u2(.clk(c1MHz), .rst(rst), .echo(Echo), .distance_count(dis));
+
 endmodule
 
-module PosCounter(clk, rst, echo, distance_count); 
-    input clk, rst, echo;
-    output[19:0] distance_count;
+module PosCounter (
+    input clk,
+    input rst,
+    input echo,
+    output [19:0] distance_count
+);
+    localparam S0 = 2'b00;
+    localparam S1 = 2'b01;
+    localparam S2 = 2'b10;
 
-    parameter S0 = 2'b00;
-    parameter S1 = 2'b01; 
-    parameter S2 = 2'b10;
-    
     wire start, finish;
-    reg[1:0] curr_state, next_state;
+    reg [1:0] curr_state, next_state;
     reg echo_reg1, echo_reg2;
-    reg[19:0] count, distance_register;
-    wire[19:0] distance_count; 
+    reg [19:0] count, distance_register;
 
-    always@(posedge clk) begin
+    always @(posedge clk) begin
         if(rst) begin
             echo_reg1 <= 0;
             echo_reg2 <= 0;
@@ -42,8 +58,8 @@ module PosCounter(clk, rst, echo, distance_count);
             curr_state <= S0;
         end
         else begin
-            echo_reg1 <= echo;   
-            echo_reg2 <= echo_reg1; 
+            echo_reg1 <= echo;
+            echo_reg2 <= echo_reg1;
             case(curr_state)
                 S0:begin
                     if (start) curr_state <= next_state; //S1
@@ -62,7 +78,7 @@ module PosCounter(clk, rst, echo, distance_count);
         end
     end
 
-    always @(*) begin
+    always @* begin
         case(curr_state)
             S0:next_state = S1;
             S1:next_state = S2;
@@ -71,59 +87,45 @@ module PosCounter(clk, rst, echo, distance_count);
         endcase
     end
 
-    assign start = echo_reg1 & ~echo_reg2;  
+    assign start = echo_reg1 & ~echo_reg2;
     assign finish = ~echo_reg1 & echo_reg2;
 
     // TODO: trace the code and calculate the distance, output it to <distance_count>
-    
+
 endmodule
 
-// send trigger signal to sensor
-module TrigSignal(clk, rst, trig);
-    input clk, rst;
-    output trig;
+module SonicTrigger (
+    input  rst,
+    input  c1MHz,
+    output trig
+);
+    reg [16:0] cnt;
 
-    reg trig, next_trig;
-    reg[23:0] count, next_count;
+    always @(posedge c1MHz, posedge rst)
+    if (rst)
+        cnt <= 0;
+    else if (cnt < 17'd100000)
+        cnt <= cnt + 1;
+    else
+        cnt <= 0;
 
-    always @(posedge clk, posedge rst) begin
-        if (rst) begin
-            count <= 0;
-            trig <= 0;
-        end
-        else begin
-            count <= next_count;
-            trig <= next_trig;
-        end
-    end
+    assign trig = !rst && (cnt < 17'd11);
+endmodule : SonicTrigger
 
-    // count 10us to set <trig> high and wait for 100ms, then set <trig> back to low
-    always @(*) begin
-        next_trig = trig;
-        next_count = count + 1;
-        // TODO: set <next_trig> and <next_count> to let the sensor work properly
-    end
-endmodule
+module ClkDivider #(
+    parameter n = 1
+)(
+    input  rst,
+    input  clk,
+    output clk_div
+);
+    reg [n - 1:0] num;
 
-// clock divider for T = 1us clock
-module div(clk ,out_clk);
-    input clk;
-    output out_clk;
-    reg out_clk;
-    reg [6:0]cnt;
-    
-    always @(posedge clk) begin   
-        if(cnt < 7'd50) begin
-            cnt <= cnt + 1'b1;
-            out_clk <= 1'b1;
-        end 
-        else if(cnt < 7'd100) begin
-	        cnt <= cnt + 1'b1;
-	        out_clk <= 1'b0;
-        end
-        else if(cnt == 7'd100) begin
-            cnt <= 0;
-            out_clk <= 1'b1;
-        end
-    end
-endmodule
+    always @(posedge clk, posedge rst)
+    if (rst)
+        num <= {n{1'b0}};
+    else
+        num <= num + 1'd1;
+
+    assign clk_div = num[n - 1];
+endmodule : ClkDivider
