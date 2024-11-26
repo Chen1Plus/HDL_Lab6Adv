e4e6f6a2c84717d9a0a388fbc9e68f53446bdd96
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-11-26 21:59:20 +0800

    sonic optimize

--- src/hdl/sonic.v
+++ src/hdl/sonic.v
@@ -1,112 +1,66 @@
-// sonic_top is the module to interface with sonic sensors
-// clk = 100MHz
-// <Trig> and <Echo> should connect to the sensor
-// <distance> is the output distance in cm
 module Sonic (
-    input         rst,
-    input         clk,
-    input         echo,
-    output        trig,
-    output [19:0] distance
+    input            rst,
+    input            clk,
+    input            echo,
+    output           trig,
+    output reg [5:0] distance // unit: cm
 );
     wire c1MHz, c8MHz;
 
-    Clk8MHz c0 (
-        .reset(rst),
-        .clk  (clk),
-        .c8MHz(c8MHz)
-    );
+    Clk8MHz             c0 (.reset(rst), .clk(clk),   .c8MHz  (c8MHz));
+    ClkDivider #(.n(3)) c1 (.rst  (rst), .clk(c8MHz), .clk_div(c1MHz));
 
-    ClkDivider c1 (
-        .rst    (rst),
-        .clk    (clk),
-        .clk_div(c1MHz)
-    );
-
-    SonicTrigger st0 (
-        .rst(rst),
-        .c1MHz(c1MHz),
-        .trig(trig)
-    );
-
-    PosCounter pc0 (
-        .rst(rst),
-        .c1MHz(c1MHz),
-        .echo(echo),
-        .distance_count(distance)
-    );
-endmodule
-
-module PosCounter (
-    input rst,
-    input c1MHz,
-    input echo,
-    output [19:0] distance_count
-);
-    localparam S0 = 2'b00;
-    localparam S1 = 2'b01;
-    localparam S2 = 2'b10;
-
-    wire start, finish;
-    reg [1:0] state;
-    // it seems echo is not necessary
-    reg echo_reg, echo_delay;
-    // it seems like 19-bits is enough
-    reg [19:0] count, distance_register;
+    reg [16:0] trig_cnt;
 
     always @(posedge c1MHz, posedge rst) begin
-        if (rst) begin
-            echo_reg <= 0;
-            echo_delay <= 0;
-            count <= 0;
-            distance_register  <= 0;
-            state <= S0;
-        end
-        else begin
-            echo_reg <= echo;
-            echo_delay <= echo_reg;
-            case(state)
-                S0:begin
-                    if (start) state <= S1; //S1
-                    else count <= 0;
-                end
-                S1:begin
-                    if (finish) state <= S2; //S2
-                    else count <= count + 1;
-                end
-                S2:begin
-                    distance_register <= count;
-                    count <= 0;
-                    state <= S0; //S0
-                end
-            endcase
-        end
+        if (rst || trig_cnt >= 17'd100000)
+            trig_cnt <= 0;
+        else
+            trig_cnt <= trig_cnt + 1;
     end
+    assign trig = !rst && (trig_cnt < 17'd11);
 
-    assign start = echo_reg & ~echo_delay;
-    assign finish = ~echo_reg & echo_delay;
+    reg  [1:0] pos_state;
+    reg [11:0] pos_cnt;
 
-    // DONE: trace the code and calculate the distance, output it to <distance_count>
-    assign distance_count = distance_register * 17 / 1000;
-endmodule
+    localparam POS_S0 = 2'b00;
+    localparam POS_S1 = 2'b01;
+    localparam POS_S2 = 2'b10;
 
-module SonicTrigger (
-    input  rst,
-    input  c1MHz,
-    output trig
-);
-    reg [16:0] cnt;
+    reg echo_delay;
+    always @(posedge c1MHz) echo_delay <= echo;
 
     always @(posedge c1MHz, posedge rst)
-    if (rst)
-        cnt <= 0;
-    else if (cnt < 17'd100000)
-        cnt <= cnt + 1;
-    else
-        cnt <= 0;
+    if (rst) begin
+        pos_state <= POS_S0;
+        pos_cnt <= 12'd0;
+    end else begin
+        pos_state <= pos_state;
+        case(pos_state)
+            POS_S0: begin
+                if (echo & ~echo_delay) pos_state <= POS_S1;
+                pos_cnt <= 12'd0;
+            end
+            POS_S1: begin
+                if (~echo & echo_delay) pos_state <= POS_S2;
+                pos_cnt <= pos_cnt + 12'd1;
+            end
+            default: begin
+                pos_state <= POS_S0;
+                pos_cnt <= pos_cnt;
+            end
+        endcase
+    end
 
-    assign trig = !rst && (cnt < 17'd11);
-endmodule : SonicTrigger
+    always @* begin
+        if (rst)
+            distance <= 0;
+        else if (pos_state == POS_S2)
+            distance <= pos_cnt * 16'd17 / 1000;
+        else
+            distance <= distance;
+    end
+endmodule
 
 module ClkDivider #(
     parameter n = 1
--- src/hdl/sonic_test.v
+++ src/hdl/sonic_test.v
@@ -1,21 +1,15 @@
 module SonicTest (
-    input rst,
-    input clk,
-
-    input  sonic_echo,
-    output sonic_trig,
-
-    output [9:0] led
+    input        rst,
+    input        clk,
+    input        sonic_echo,
+    output       sonic_trig,
+    output [5:0] led
 );
-    wire [19:0] distance;
-
     Sonic s0 (
         .rst     (rst),
         .clk     (clk),
         .echo    (sonic_echo),
         .trig    (sonic_trig),
-        .distance(distance)
+        .distance(led)
     );
-
-    assign led = distance[11:2];
 endmodule : SonicTest
