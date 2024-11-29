cde9c91eee3057628c7e4a4b07cc50ea01a69206
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-11-29 14:19:13 +0800

    sonic fix

--- src/hdl/lab6_advanced.v
+++ src/hdl/lab6_advanced.v
@@ -67,7 +67,7 @@ module lab6_advanced (
         .rst    (rst),
         .c100MHz(clk),
         .dir    (state),
-        .speed  (distance > 10 ? 10'd820 : 10'd0),
+        .speed  (distance > 8 || distance < 2 ? 10'd820 : 10'd0),
         .in     (motor_in),
         .pwm_ab (motor_pwm_ab)
     );
